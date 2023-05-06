8e3fc2e65e123ce4575a6598dcb2c192f4d9486a
Zainab-Alzubair <zainab.a.elzubaie@gmail.com>
2023-05-06 12:56:50 +0300

    Setup the frontend

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,14 @@
 // Entry point for the build script in your package.json
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import store from './redux/store';
+import { Provider } from 'react-redux';
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
